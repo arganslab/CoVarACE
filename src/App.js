a34ba7310ac0a5380f0217512337ec05948f7449
Faris <farisfizal@gmail.com>
2021-03-28 18:49:20 -0600

    OTHER: Remove instructions list

--- src/App.js
+++ src/App.js
@@ -52,14 +52,17 @@ export default class App extends Component {
             critical role in human ACE2 recognition. Through CoVarACE, we describe the changes in binding affinity and the key 
             residues related to the complexes of ACE2 with {'>'}50 RBD mutants assessed using molecular dynamics simulation.
           </p>
-          <ul>
-            <li>Select a variant from the dropdown below to view its associated per-residue free energy decomposition.</li>
-            <li>Filter variants by country or continent by typing in the variant dropdown/search bar.</li>
-            <li>Use mouse or trackpad controls to interact with the structure visualization viewport.</li>
-            <li>Use the available toggles on the free energy decomposition plot to highlight specific data points and save the plot to an image.</li>
-            <li>Customize the representation of the Spike-ACE2 complex through the representation dropdown.</li>
-            <li>[in development] Adjust the threshold to view key residues with an energy contribution below this value.</li>
-          </ul>
+          {
+            false &&
+            <ul>
+              <li>Select a variant from the dropdown below to view its associated per-residue free energy decomposition.</li>
+              <li>Filter variants by country or continent by typing in the variant dropdown/search bar.</li>
+              <li>Use mouse or trackpad controls to interact with the structure visualization viewport.</li>
+              <li>Use the available toggles on the free energy decomposition plot to highlight specific data points and save the plot to an image.</li>
+              <li>Customize the representation of the Spike-ACE2 complex through the representation dropdown.</li>
+              <li>[in development] Adjust the threshold to view key residues with an energy contribution below this value.</li>
+            </ul>
+          }
         </div>
         <VariantSelect 
           handleVariantSelect={(selection) => this.handleVariantSelect(selection) }
