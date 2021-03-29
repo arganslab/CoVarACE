f4d37a3154e56fab35cfd216536a417c42cbe208
Faris <farisfizal@gmail.com>
2021-03-28 18:47:47 -0600

    DESIGN: Update header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         <div className='header'>
             <img src={headerLogo} alt='CoVarACE logo' className='header-logo' />
-            <h2 className='header-desc'>COVID19 Variants-ACE2 Predicted Binding Affinity Database</h2>
+            <h2 className='header-desc'><b>C</b>OVID19 <b>V</b>ariants-<b>ACE</b>2 Predicted Binding Affinity Database</h2>
         </div>
     );
 }
